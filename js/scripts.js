e1b9ade76d0e096c897eb24c01667f15e13142c7
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-11-09 05:51:54 +0500

    Correct js validation

--- js/scripts.js
+++ js/scripts.js
@@ -6,39 +6,52 @@ var elRainingSwitch = elForm.querySelector('.js-raining-switch');
 var elGymSwitch = elForm.querySelector('.js-gym-switch');
 
 
-elForm.addEventListener('submit', function (evt) {
+var run = function (evt) {
   evt.preventDefault();
   
   var inputTemperature = parseInt(elTemperatureInput.value.trim(), 10);
 
+  // O'zgaruvchilar yasab olish
+  var minTemperatureForRun = '5';
+  var maxTemperatureForRun = '30';
+  var normalTemperatureForRun = minTemperatureForRun <= inputTemperature && inputTemperature <= maxTemperatureForRun;
+  var statusOfRain = elRainingSwitch;
+  var statusOfGym = elGymSwitch;
+  
   // Qolib ketgan classlarni olib tashlash
+    elResult.classList.remove('alert-success', 'alert-danger', 'alert-warning');
   
-  elResult.classList.remove('alert-success', 'alert-danger', 'alert-warning');
-  
-  // Hozircha kerakmas bo'lib turibti 
+
+  // Hozircha kerak emas bo'lib turibti 
   // if (elTemperatureInput.length === 0) {
   //   elResult.classList.add('alert-warning');
   //   return;
   // } 
 
+
   // Bo'shligini yoki son kiritilmaganini tekshirish
-  
-  if (isNaN(inputTemperature)) {
+    if (isNaN(inputTemperature)) {
     elResult.classList.add('alert-warning');
     elResult.textContent = 'Please input numbers';
     return;
   } 
-
-  if (elRainingSwitch.checked) {
-    console.log('Hello');
-  }
-
+ 
   
-  if (5 <= inputTemperature && inputTemperature <= 30) {
+  if (normalTemperatureForRun && !statusOfRain.checked) {
     elResult.classList.add('alert-success');
+    elResult.textContent = 'Yes, I will.';
+  } else if (normalTemperatureForRun && statusOfRain.checked && statusOfGym.checked) {
+    elResult.classList.add('alert-success');
+    elResult.textContent = 'Yes, I will.';
   } else {
     elResult.classList.add('alert-danger');
+    elResult.textContent = "No, I won't.";
   }
-  elResult.textContent = inputTemperature;
+  // elResult.textContent = inputTemperature;
   
-});
+};
+
+elForm.addEventListener('submit', run);
+elTemperatureInput.addEventListener('blur', run);
+elRainingSwitch.addEventListener('change', run);
+elGymSwitch.addEventListener('change', run);
